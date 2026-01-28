69b4e8c5e5f1d4f7ef57a3cf3aeb71b3f039a5a6
vikrant singh <jadounvikrant@example.com>
2026-01-28 23:41:48 +0530

    Remove unused public folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my Macfolio</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to my Macfolio</h1>
     </div>
   )
 }
